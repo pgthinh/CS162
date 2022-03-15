7be7bf1d0da6683ce4f5f56f1d509ac3c4d743d8
ngvahieu <vanhieu230303@gmail.com>
2022-03-15 23:34:01 +0700

    Update FindID

--- FindAndGet_ID.cpp
+++ FindAndGet_ID.cpp
@@ -4,22 +4,24 @@ bool Find_Course(string CourseId)
 {
 	Course* first_course = new Course;
 	Course* course_cur = first_course;
-	string path = 'Data /' + to_string(getCurrentYear()) + '/' + to_string(getCurrentSemester()) + '/courses/courseList.txt';
+	string path = 'DATA /' + to_string(getCurrentYear()) + '/' + to_string(getCurrentSemester()) + '/courses/courseList.txt';
 	ifstream fin(path);
-	fin >> course_cur->CourseID;
-	while (!fin.eof()) {
-		course_cur->next_course = new Course;
+	while (fin >> course_cur->CourseID) {
+		course_cur->next_Course = new Course;
+		course_cur->next_Course->previous_Course = course_cur;
 		course_cur = course_cur->next_course;
-		fin >> course_cur->CourseID;
 	}
-	course_cur->next_course = nullptr;
+	Course* temp = course_cur;
+	course_cur = course_cur->previous_Course;
+	course_cur->next_Course = nullptr;
+	delete temp;
     fin.close();
 	course_cur = first_course;
 	while (course_cur) {
 		if (course_cur->CourseID == CourseID) {
 			return true;
 		}
-		course_cur = course_cur->next_course;
+		course_cur = course_cur->next_Course;
 	}
 	return false;
 }
@@ -27,7 +29,7 @@ bool Find_ID(string CourseID, string student_id)
 {
 	if (!Find_Course(CourseID)) return false;
 	string id;
-	string path = 'Data /' + to_string(getCurrentYear()) + '/' + to_string(getCurrentSemester()) + '/courses/' + CourseID + '/StudentIDList.txt';
+	string path = 'Data /' + to_string(getCurrentYear()) + '/' + to_string(getCurrentSemester()) + '/courses/' + CourseID + '/Students.txt';
 	ifstream fin(path);
 	while (fin >> id) {
 		if (id == student_id) break;
