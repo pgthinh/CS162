1fe44feb3418f5f36d4f5f9ad6ac0a5cc8eeddf0
ngvahieu <vanhieu230303@gmail.com>
2022-03-17 14:38:18 +0700

    Delete Doubly Linked List

---
+++ DeleteList.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include "struct.hpp"
+using namespace std;
+
+void DeleteClass(Class*& class){
+    while(class){
+        Class* temp = class;
+        class = class->next_Class;
+        delete temp;
+    }
+}
+void DeleteStudent(Student*& student){
+    while(student){
+        Student* temp = student;
+        student = student->next_Student;
+        delete temp;
+    }
+}
