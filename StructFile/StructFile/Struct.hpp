68930a2cb8eb068636073cd909f29cf432fd2d88
ngvahieu <vanhieu230303@gmail.com>
2022-03-18 22:26:35 +0700

    Update Struct

--- StructFile/StructFile/Struct.hpp
+++ StructFile/StructFile/Struct.hpp
@@ -52,10 +52,11 @@ struct Class {
 struct Student {
     int No;
     string ID; // Username when login to the system
-    string Password;
+    //string Password;
     string Name;
     string Gender;
     Time Dob;
+    string socialID;
     Student* previous_Student = NULL;
     Student* next_Student = NULL;
     Student(int No, string ID, string Password, string Name, string Gender, Time Dob) :
