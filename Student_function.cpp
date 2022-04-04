3494001b2dc3ea057218d395d40558339358063b
Gia-Thinh Pham <87513296+pgthinh@users.noreply.github.com>
2022-04-04 08:17:17 +0700

    Update Student_function.cpp

--- Student_function.cpp
+++ Student_function.cpp
@@ -1,5 +1,6 @@
-﻿#include "Student.h"
-// Hàm khởi tạo list ban đầu
+#include "Student.h"
+#include "Console.h"
+
 void InnitList(List& l)
 {
     l.pHead = l.pTail = NULL;
@@ -61,53 +62,65 @@ void AddCourse(List& l)
         FileIn >> course[i].SecondDayOfWeek >> course[i].SecondSessionOfWeek;
         FileIn.close();
     }
-    for (int i = 0; i < numbers_of_course; i++)
-    {
-        cout << i + 1 << endl;
-        cout << "course ID = " << course[i].CourseID << endl;
-        cout << "course name  = " << course[i].CourseName << endl;
-        cout << "course instructor = " << course[i].TeacherName << endl;
-        cout << "Numbers of credits = " << course[i].NumberOfCredits << endl;
-        cout << "Maximum student = " << course[i].MaxNumOfStudent<< endl;
-        cout << "first section = " << course[i].FirstDayOfWeek <<" "<< course[i].FirstSessionOfWeek << endl;
-        cout << "second section = "<<course[i].SecondDayOfWeek<<" "<<course[i].SecondSessionOfWeek<<endl;
-    }
-    cout << "---------------------------" << endl;
+
     cin.ignore();
     string choice;
     while (true)
     {
-        cout << "Input the course ID " << endl;
-        cout << "If you dont want to choose any more , press 0 " << endl;
-        cout << "Your choice = "; getline(cin,choice);
-        cout << "---------------------" << endl;
+        clrscr(); //Heading();
+        gotoxy(2, 12); cout << "No";
+        gotoxy(6, 12); cout << " Course ID";
+        gotoxy(6, 13); cout << " ";
+        gotoxy(17, 12); cout << " Course name";
+        gotoxy(17, 13); cout << " ";
+        gotoxy(54, 12); cout << " Instructor";
+        gotoxy(54, 13); cout << " ";
+        gotoxy(77, 12); cout << " Number of";
+        gotoxy(77, 13); cout << " credits";
+        gotoxy(89, 12); cout << " Maximum";
+        gotoxy(89, 13); cout << " student";
+        gotoxy(99, 12); cout << " First";
+        gotoxy(99, 13); cout << " session";
+        gotoxy(109, 12); cout << " Second";
+        gotoxy(109, 13); cout << " session";
+        for (int i = 0; i < numbers_of_course; ++i) {
+            gotoxy(2, 15 + i * 2); cout << i + 1 << ". ";
+            gotoxy(6 + 1, 15 + i * 2); cout << course[i].CourseID;
+            gotoxy(17 + 1, 15 + i * 2); cout << course[i].CourseName;
+            gotoxy(54 + 1, 15 + i * 2); cout << course[i].TeacherName;
+            gotoxy(77 + 1, 15 + i * 2); cout << course[i].NumberOfCredits;
+            gotoxy(89 + 1, 15 + i * 2); cout << course[i].MaxNumOfStudent;
+            gotoxy(99 + 1, 15 + i * 2); cout << course[i].FirstDayOfWeek << '-' << course[i].FirstSessionOfWeek;
+            gotoxy(109 + 1, 15 + i * 2); cout << course[i].SecondDayOfWeek << '-' <<course[i].SecondSessionOfWeek;
+        }
+        cout << "\n\n";
+        for (int rep = 1; rep <= 120; rep++) cout << char(220); cout << endl;
+        cout << "\n\t\t\t\t\t Input the course ID (0 to return): ";
+        getline(cin,choice);
         for (int i = 0; i < choice.length(); i++)
-        {
             choice[i] = toupper(choice[i]);
-        }
-        if (choice == "0")
-        {
-            break;
-        }
-        bool Check = false; // kiem tra xem course da chon hay chua ? neu chua chon thi duoc add 
+        if (choice == "0") break;
+        bool Check = false; // kiem tra xem course da chon hay chua ? neu chua chon thi duoc add
         Node* node = l.pHead;
+
         while (node)
         {
-            if (node->course.CourseID == choice)
+             if (node->course.CourseID == choice)
             {
                 Check = true;
                 break;
             }
             node = node->pNext;
         }
+
+        bool Check1 = false; // check xem môn chọn co bi trùng giờ các môn trước hay không ?
         if (!Check)
         {
-            for (int i = 0; i < numbers_of_course; i++)
+            for (int i = 0; i < numbers_of_course; ++i)
             {
                 if (course[i].CourseID == choice)
                 {
                     Node* node = l.pHead;
-                    bool Check1 = false; // check xem môn chọn co bi trùng giờ các môn trước hay không ?
                     while (node)
                     {
                         if (course[i].FirstDayOfWeek == node->course.FirstDayOfWeek || course[i].SecondDayOfWeek == node->course.SecondDayOfWeek)
@@ -120,33 +133,38 @@ void AddCourse(List& l)
                     }
                     if (!Check1)
                     {
-                        cout << "----------------------------" << endl;
-                        cout << "Add course successful" << endl;
-                        cout << "----------------------------" << endl;
+                        SetColor(10);
+                        cout << "\n\t\t\t\t\t\tAdd course successfully  " << endl;
+                        SetColor(15);
+                        delay(1500);
                         Add_Course(l, course[i]);
+                        Check1 = true;
                         break;
                     }
                     else
                     {
-                        cout << "----------------------------" << endl;
-                        cout << "Bi trung h nen khong the them duoc nua" << endl;
-                        cout << "----------------------------" << endl;
+                        SetColor(12);
+                        cout << "\t\t\t\t You already have another the course at the same time" << endl;
+                        SetColor(15);
+                        delay(1500);
                         break;
                     }
                 }
-                else
-                {
-                    cout << "----------------------------" << endl;
-                    cout << "Can not find any course with ID = " << choice << endl;
-                    cout << "----------------------------" << endl;
-                }
+
+            }
+            if (!Check1){
+                SetColor(12);
+                cout << "\n\t\t\t\t\t Can not find any course with ID: " << choice;
+                SetColor(15);
+                delay(1500);
             }
         }
         else
         {
-            cout << "----------------------------" << endl;
-            cout << "You can not insert this course anymore" << endl;
-            cout << "----------------------------" << endl;
+            SetColor(12);
+            cout << "\n\t\t\t\t\t   You can not add this course anymore";
+            SetColor(15);
+            delay(1500);
         }
     }
 }
@@ -160,14 +178,20 @@ void Read_My_Course_From_TXT(List& l)
     {
         Course course;
         getline(FileIn, course.CourseID);
+      //  cout << course.CourseID << " ";
         getline(FileIn, course.CourseName);
         getline(FileIn, course.TeacherName);
         FileIn >> course.NumberOfCredits;
         FileIn >> course.MaxNumOfStudent;
-        FileIn >> course.FirstDayOfWeek >> course.FirstSessionOfWeek;
-        FileIn >> course.SecondDayOfWeek >> course.SecondSessionOfWeek;
+        FileIn.ignore(1);
+        getline(FileIn,course.FirstDayOfWeek,' ');
+        getline(FileIn,course.FirstSessionOfWeek,'\n');
+        getline(FileIn,course.SecondDayOfWeek,' ');
+        getline(FileIn,course.SecondSessionOfWeek,'\n');
         numbers_of_course++;
+     //   cout << "1" << " ";
     }
+
     FileIn.close();
     FileIn.open("MyCourse.txt", ios_base::in);
     Course* course = new Course[numbers_of_course];
@@ -178,9 +202,14 @@ void Read_My_Course_From_TXT(List& l)
         getline(FileIn, course[i].TeacherName);
         FileIn >> course[i].NumberOfCredits;
         FileIn >> course[i].MaxNumOfStudent;
-        FileIn >> course[i].FirstDayOfWeek >> course[i].FirstSessionOfWeek;
-        FileIn >> course[i].SecondDayOfWeek >> course[i].SecondSessionOfWeek;
+        FileIn.ignore(1);
+        getline(FileIn,course[i].FirstDayOfWeek,' ');
+        getline(FileIn,course[i].FirstSessionOfWeek,'\n');
+        getline(FileIn,course[i].SecondDayOfWeek,' ');
+        getline(FileIn,course[i].SecondSessionOfWeek,'\n');
     }
+
+    InnitList(l);
     for (int i = 0; i < numbers_of_course; i++)
     {
         Add_Course(l, course[i]);
@@ -190,50 +219,72 @@ void Read_My_Course_From_TXT(List& l)
 // Show thông tin về những course đã chọn
 void Show_My_course(const List &l)
 {
+    clrscr();
     if (!l.pHead)
     {
-        cout << "----------------------------" << endl;
-        cout << "You have not registered any course" << endl;
-        cout << "----------------------------" << endl;
+        SetColor(12);
+        cout << "\t\t\t\t\t   You have not registered any course";
+        SetColor(15);
     }
     else
     {
         Node* node = l.pHead;
-        int count = 0;
+        int i = 0;
+        gotoxy(2, 12); cout << "No";
+        gotoxy(6, 12); cout << " Course ID";
+        gotoxy(6, 13); cout << " ";
+        gotoxy(17, 12); cout << " Course name";
+        gotoxy(17, 13); cout << " ";
+        gotoxy(54, 12); cout << " Instructor";
+        gotoxy(54, 13); cout << " ";
+        gotoxy(77, 12); cout << " Number of";
+        gotoxy(77, 13); cout << " credits";
+        gotoxy(89, 12); cout << " Maximum";
+        gotoxy(89, 13); cout << " student";
+        gotoxy(99, 12); cout << " First";
+        gotoxy(99, 13); cout << " session";
+        gotoxy(109, 12); cout << " Second";
+        gotoxy(109, 13); cout << " session";
         while (node)
         {
-            cout << "Course ID = " << node->course.CourseID << endl;
-            cout << "Course name = " << node->course.CourseName << endl;
-            cout << "Course Instructor = " << node->course.TeacherName << endl;
-            cout << "Numbers of credits = " << node->course.NumberOfCredits << endl;
-            cout << "Maximum student = " << node->course.MaxNumOfStudent << endl;
-            cout << "first section = " << node->course.FirstDayOfWeek <<" "<<node->course.FirstSessionOfWeek << endl;
-            cout << "second section = " << node->course.SecondDayOfWeek << node->course.SecondSessionOfWeek << endl;
-            cout << "----------------------------" << endl;
-            count++;
+            gotoxy(2, 15 + i * 2); cout << i+ 1 << ". ";
+            gotoxy(6 + 1, 15 + i * 2); cout << node->course.CourseID ;
+            gotoxy(17 + 1, 15 + i * 2); cout << node->course.CourseName;
+            gotoxy(54 + 1, 15 + i * 2); cout << node->course.TeacherName;
+            gotoxy(77 + 1, 15 + i * 2); cout << node->course.NumberOfCredits ;
+            gotoxy(89 + 1, 15 + i * 2); cout << node->course.MaxNumOfStudent ;
+            gotoxy(99 + 1, 15 + i * 2); cout << node->course.FirstDayOfWeek << " " << node->course.FirstSessionOfWeek;
+            gotoxy(109 + 1, 15 + i * 2); cout << node->course.SecondDayOfWeek << " " << node->course.SecondSessionOfWeek;
+            i++;
             node = node->pNext;
         }
+        cout << "\n\n";
+        for (int rep = 1; rep <= 120; rep++) cout << char(220); cout << endl;
     }
 }
 // Hàm xoá course ra khỏi những gì đã chọn
 void Remove_A_Course(List& l)
 {
+    clrscr();// Heading();
+    cout << "\n\n\t\t\t\t\t       ";
+    for (int rep = 1; rep <= 5; rep++) cout << char(219); cout << " REMOVE A COURSE "; for (int rep = 1; rep <= 5; rep++) cout << char(219);
+    cout << "\n\n";
     if (!l.pHead)
     {
-        cout << "----------------------------" << endl;
-        cout << "You dont have any course to remove" << endl;
-        cout << "----------------------------" << endl;
+        SetColor(12);
+        cout << "\t\t\t\t\t   You dont have any course to remove";
+        delay(1500);
+        SetColor(15);
     }
     else
     {
         Show_My_course(l);
         string name;
         cin.ignore();
-        cout << "Input course shortened name you want to remove = "; getline(cin,name);
+        cout << "\n\n\t\t\t\t\t\t\  Course ID: ";
+        getline(cin,name);
         for (int i = 0; i < name.length(); i++)
-        {
             name[i] = toupper(name[i]);
-        }
         Node* node = l.pHead;
         if (l.pHead->course.CourseID== name)
         {
@@ -260,9 +311,10 @@ void Remove_A_Course(List& l)
             node->pNext = node->pNext->pNext;
             delete node->pNext;
         }
-        cout << "----------------------------" << endl;
-        cout << "Removed Course Successfully" << endl;
-        cout << "----------------------------" << endl;
+        SetColor(10);
+        cout << "\t\t\t\t\t  Removed Course Successfully" << endl;
+        SetColor(15);
+        delay(1500);
     }
 }
 // Hàm ghi course ra file
@@ -277,7 +329,7 @@ void Write_To_My_Course(const List &l)
         count ++;
         node = node->pNext;
     }
-    FileOut << count << endl;
+  //  FileOut << count << endl;
     node = l.pHead;
     while (node)
     {
@@ -287,7 +339,8 @@ void Write_To_My_Course(const List &l)
         FileOut << node->course.NumberOfCredits << endl;
         FileOut << node->course.MaxNumOfStudent << endl;
         FileOut << node->course.FirstDayOfWeek << " " << node->course.FirstSessionOfWeek << endl;
-        FileOut << node->course.SecondDayOfWeek<<" "<<node->course.SecondSessionOfWeek << endl;
+        if (node->pNext == NULL) FileOut << node->course.SecondDayOfWeek<<" "<<node->course.SecondSessionOfWeek;
+        else FileOut << node->course.SecondDayOfWeek<<" "<<node->course.SecondSessionOfWeek << endl;
         node = node->pNext;
     }
     FileOut.close();
@@ -297,58 +350,77 @@ void Menu(List& l)
     int choice;
     while (true)
     {
-        system("cls");
-        cout << "STUDENT" << endl;
-        cout << "1.Add course" << endl;
-        cout << "2.Remove course" << endl;
-        cout << "3.My courses" << endl;
-        cout << "4.My scoreboard" << endl;
-        cout << "5.Change password" << endl; // function này Thịnh làm
-        cout << "6.View profile" << endl;
-        cout << "7.log out" << endl;
-        cout << "------------------" << endl;
-        cout << "Input your choice = "; cin >> choice;
-        if (choice == 7)
-        {
-            cout << "See you next time" << endl;
-            system("pause");
-            break;
-        }
-        else if (choice == 1)
-        {
-            AddCourse(l);
-            Write_To_My_Course(l);
-            system("pause");
-        }
-        else if (choice == 2)
-        {
-            Remove_A_Course(l);
-            Write_To_My_Course(l);
-            system("pause");
-        }
-        else if (choice == 3)
-        {
-            //Read_My_Course_From_TXT(l);
-            Show_My_course(l);
-            system("pause");
-        }
-        else if (choice == 4)
-        {
-            My_Score(l);
-            system("pause");
-        }
-        else if (choice == 6)
-        {
-            Show_Profile();
-            system("pause");
+        clrscr(); //Heading();
+        cout << "\n\n\t\t\t\t\t\t";
+        for (int rep = 1; rep <= 5; rep++) cout << char(219); cout << " STUDENT MENU "; for (int rep = 1; rep <= 5; rep++) cout << char(219);
+        cout << "\n\n";
+        cout << "\t\t\t\t\t\t   1. Add a course\n\n";
+        cout << "\t\t\t\t\t\t   2. Remove a course\n\n";
+        cout << "\t\t\t\t\t\t   3. My courses\n\n";
+        cout << "\t\t\t\t\t\t   4. My scoreboard\n\n";
+        cout << "\t\t\t\t\t\t   5. Change password\n\n";
+        cout << "\t\t\t\t\t\t   6. View profile\n\n";
+        cout << "\t\t\t\t\t\t   7. Logout\n\n";
+        for (int rep = 1; rep <= 120; rep++) cout << char(220); cout << endl;
+
+        cout << "\t\t\t\t\t\t Selection: "; cin >> choice;
+        switch (choice){
+            case 7:{
+                SetColor(10);
+                cout << "See you next time" << endl;
+                delay(1500);
+                SetColor(15);
+                break;
+            }
+            case 1:{
+                AddCourse(l);
+                Write_To_My_Course(l);
+                system("pause");
+                break;
+            }
+            case 2:{
+                Remove_A_Course(l);
+                Write_To_My_Course(l);
+                system("pause");
+                break;
+            }
+            case 3:{
+                Read_My_Course_From_TXT(l);
+                Show_My_course(l);
+                system("pause");
+                break;
+            }
+            case 4:{
+                My_Score(l);
+                system("pause");
+                break;
+            }
+            case 6:{
+                Show_Profile();
+                system("pause");
+                break;
+            }
         }
     }
 }
-// Hàm xem điểm từ file - Check tên người dùng 
+// Hàm xem điểm từ file - Check tên người dùng
 void My_Score(const List &l)
 {
     ifstream FileIn;
     string pathfixed = "csvFile";
+    clrscr(); //Heading();
+    cout << "\n\n\t\t\t\t\t\t ";
+    for (int rep = 1; rep <= 5; rep++) cout << char(219); cout << " Scoreboard "; for (int rep = 1; rep <= 5; rep++) cout << char(219);
+    cout << "\n\n";
+    int based = 5; cout.precision(2);
+    gotoxy(10 + based, 12); cout << "No";
+    gotoxy(15 + based, 12); cout << "Course ID";
+    gotoxy(27 + based, 12); cout << "Course name";
+    gotoxy(67 + based, 12); cout << "Total";
+    gotoxy(77 + based, 12); cout << "Final";
+    gotoxy(87 + based, 12); cout << "Midterm";
+    gotoxy(97 + based, 12); cout << "Other";
+    
     int ID;
     cout << "Please input your ID = "; cin >> ID;
 
@@ -356,16 +428,12 @@ void My_Score(const List &l)
     cout << "Input year = "; cin >> year;
     int semester;
     cout << "Input semester = "; cin >> semester;
-   
+
     string str2;
     cin.ignore();
     cout << "Input course name = "; getline(cin, str2);
-    for (int i = 0; i < str2.length(); i++)
-    {
-        str2[i] = toupper(str2[i]);
-    }
- 
-    string strpath = pathfixed + "/" + to_string(year) + "/" + to_string(year) + "_" + to_string(semester) + "/" + str2 + "16" + to_string(semester) + ".csv";
+
+    string strpath = pathfixed + "/" + to_string(year) + "/" + to_string(semester) + "/" + str2 + ".csv";
 
     FileIn.open(strpath, ios_base::in);
     string str3;
@@ -418,7 +486,7 @@ void Show_Profile()
     cout << "Please input your ID = "; cin >> ID;
     int class1;
     cout << "Which class are you in ? "; cin >> class1;
-    
+
     string strpath = "CLASS/21APCS" + to_string(class1) + "/" + to_string(ID) + ".txt";
 
     FileIn.open(strpath, ios_base::in);
